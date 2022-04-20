4e47a9c63b8562e2b626c62a54a403760ac3b9e0
Danilo Costa <danilocostaluiz@hotmail.com>
2022-04-20 15:10:28 -0300

    now playing suggest app

---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey.shade900,
+        appBar: AppBar(
+          title: const Text(
+            'What movie now playing can you suggest?',
+            style: TextStyle(color: Colors.amber),
+          ),
+          backgroundColor: Colors.blueGrey.shade900,
+          centerTitle: true,
+        ),
+        body: const MagicBallPage(),
+      ),
+    );
+  }
+}
+
+class MagicBallPage extends StatefulWidget {
+  const MagicBallPage({Key? key}) : super(key: key);
+
+  @override
+  State<MagicBallPage> createState() => _MagicBallPageState();
+}
+
+class _MagicBallPageState extends State<MagicBallPage> {
+  int index = -1;
+  List nowPlaying = [];
+
+  getMovies() async {
+    final response = await Dio().get(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=631c41f7f6ce868918b5a484565a830d');
+    nowPlaying = response.data['results'];
+    print(
+        '${nowPlaying.length} + ${nowPlaying[nowPlaying.length - 1]['title']}');
+  }
+
+  changeIndex() {
+    setState(() {
+      index = Random().nextInt(nowPlaying.length - 1);
+      print(index);
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setState(() {
+      getMovies();
+      // print(
+      //     '${nowPlaying.length} + ${nowPlaying[nowPlaying.length - 1]['title']}');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeIndex();
+              },
+              child: index != -1
+                  ? Image.network(
+                      'https://image.tmdb.org/t/p/w500${nowPlaying[index]['poster_path']}')
+                  : Image.asset(
+                      'images/playnowbutton1.png',
+                      width: 200,
+                    ),
+            ),
+          ),
+          if (nowPlaying.length != 0)
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: ListTile(
+                leading: Icon(Icons.movie),
+                title: Text(nowPlaying[index]['title']),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
